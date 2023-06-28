9cc260e6f31fc2a9cf1417410751aa80a4d4f9ff
christophe Dubois <dubois51ch@gmail.com>
2023-06-28 17:18:21 +0200

    sql

--- backend/database.sql
+++ backend/database.sql
@@ -1,10 +1,10 @@
-CREATE TABLE `admin` (
+CREATE TABLE admin (
     id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     username VARCHAR(80) NOT NULL,
-    hashedpassword VARCHAR(255) NOT NULL,
+    hashedpassword VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE `phone` (
+CREATE TABLE phone (
     id INT PRIMARY KEY AUTO_INCREMENT,
     brand VARCHAR(80),
     phone_name VARCHAR(80),
@@ -13,9 +13,9 @@ CREATE TABLE `phone` (
     dimensions VARCHAR(255),
     network VARCHAR(20),
     ram VARCHAR(20),
-    image_phone VARCHAR(80)
+    image VARCHAR(255)
 );
 
-INSERT INTO `phone` (brand, phone_name, version_os, storage, dimensions, network, ram, image_phone) VALUES ('Samsung', 'Galaxy S9', 'Android 8.0 Oreo', '64', '5.8', '3G/4G', '4', 'https://www.backmarket.fr/cdn-cgi/image/format%3Dauto%2Cquality%3D75%2Cwidth%3D1920/https://d1eh9yux7w8iql.cloudfront.net/product_images/154214_0a804472-8918-4908-a6de-b8396f7773b0.jpg');
+INSERT INTO `phone` (brand, phone_name, version_os, storage, dimensions, network, ram, image) VALUES ('Samsung', 'Galaxy S9', 'Android 8.0 Oreo', '64', '5.8', '3G/4G', '4', 'https://www.backmarket.fr/cdn-cgi/image/format%3Dauto%2Cquality%3D75%2Cwidth%3D1920/https://d1eh9yux7w8iql.cloudfront.net/product_images/154214_0a804472-8918-4908-a6de-b8396f7773b0.jpg');
 
-INSERT INTO `phone` (brand, phone_name, image_phone) VALUES ('Samsung', 'Galaxy S23 Ultra', 'https://www.backmarket.fr/cdn-cgi/image/format%3Dauto%2Cquality%3D75%2Cwidth%3D1920/https://d1eh9yux7w8iql.cloudfront.net/product_images/3ac876fa-5e26-4dea-bfb6-809e528e21eb-1_7e1f9216-8fec-463f-9a16-737dd68338c7.jpg');
+INSERT INTO `phone` (brand, phone_name, image) VALUES ('Samsung', 'Galaxy S23 Ultra', 'https://www.backmarket.fr/cdn-cgi/image/format%3Dauto%2Cquality%3D75%2Cwidth%3D1920/https://d1eh9yux7w8iql.cloudfront.net/product_images/3ac876fa-5e26-4dea-bfb6-809e528e21eb-1_7e1f9216-8fec-463f-9a16-737dd68338c7.jpg');
