0243b895c7d076fa0a25872bf3bb052e9cd9fe05
christophe Dubois <dubois51ch@gmail.com>
2023-06-29 11:17:02 +0200

    css modif

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,8 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Nav from "./components/Nav";
-import Liste from "./components/Liste";
 import Footer from "./components/Footer";
 import Login from "./pages/Login";
-import Characteristic from "./components/Characteristic";
 import "./App.scss";
 
 import FilterBar from "./components/FilterBar";
@@ -14,11 +12,34 @@ function App() {
     <div className="App">
       <Nav />
       <FilterBar />
-      <Characteristic />
-      <Liste />
       <Routes>
         <Route path="/login" element={<Login />} />
       </Routes>
+      <div>
+        <h1>Bonjour</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate,
+          architecto officia magnam labore alias nesciunt qui tempora omnis
+          molestiae cumque aut, modi maiores fuga nisi dolorem est quae totam!
+          Eius.
+        </p>
+        <img
+          src="https://www.backmarket.fr/cdn-cgi/image/format%3Dauto%2Cquality%3D75%2Cwidth%3D640/https://d1eh9yux7w8iql.cloudfront.net/product_images/512583_4d813e9a-b1b4-4d25-b40f-4b770111d865.jpg"
+          alt="Phone"
+        />
+        <img
+          src="https://www.backmarket.fr/cdn-cgi/image/format%3Dauto%2Cquality%3D75%2Cwidth%3D640/https://d1eh9yux7w8iql.cloudfront.net/product_images/512583_4d813e9a-b1b4-4d25-b40f-4b770111d865.jpg"
+          alt="Phone"
+        />{" "}
+        <img
+          src="https://www.backmarket.fr/cdn-cgi/image/format%3Dauto%2Cquality%3D75%2Cwidth%3D640/https://d1eh9yux7w8iql.cloudfront.net/product_images/512583_4d813e9a-b1b4-4d25-b40f-4b770111d865.jpg"
+          alt="Phone"
+        />{" "}
+        <img
+          src="https://www.backmarket.fr/cdn-cgi/image/format%3Dauto%2Cquality%3D75%2Cwidth%3D640/https://d1eh9yux7w8iql.cloudfront.net/product_images/512583_4d813e9a-b1b4-4d25-b40f-4b770111d865.jpg"
+          alt="Phone"
+        />
+      </div>
       <Footer />
     </div>
   );
